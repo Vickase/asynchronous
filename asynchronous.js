311b5b550ad58cc05e7c51e103909a52632d7939
Victoria Aseff <vickase.a@gmail.com>
2019-12-26 14:16:31 -0300

    first 3 exercises

--- asynchronous.js
+++ asynchronous.js
@@ -5,18 +5,10 @@
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 1');
-// ...your code below
-
-
-
-
-
-console.log('End of Challenge 1');
+console.log(" 'I am at the beginning of the code'.");
+setTimeout(() => console.log("I am in the setTimeout callback function"), 3000);
+console.log("I am at the end of the code'");
 // */// (do not alter this line)
-
-
-
 
 /////////////////
 //             //
@@ -25,11 +17,9 @@ console.log('End of Challenge 1');
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 2');
+console.log("Start of Challenge 2");
 // ...your code below
-
-
-
+const id = setInterval(() => console.log("Interval Hello!"), 2000);
 
 // ...your code above
 function clearAllIntervals() {
@@ -37,10 +27,15 @@ function clearAllIntervals() {
     clearInterval(i);
   }
 }
-console.log('End of Challenge 2');
+setTimeout(() => clearInterval(id), 10000);
+// ...your code above
+function clearAllIntervals() {
+  for (let i = 0; i < 1000; i++) {
+    clearInterval(i);
+  }
+}
+console.log("End of Challenge 2");
 // */// (do not alter this line)
-
-
 
 /////////////////
 //             //
@@ -49,18 +44,21 @@ console.log('End of Challenge 2');
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 3');
+console.log("Start of Challenge 3");
 // ...your code below
+function everyXsecsForYsecs(fn, interval, totalTime) {
+  const id = setInterval(fn, interval * 1000);
+  setTimeout(() => clearInterval(id), totalTime * 1000);
+}
 
+function sayHowdy() {
+  console.log("Howdy");
+}
 
+everyXsecsForYsecs(sayHowdy, 1, 5);
 
-
-
-console.log('End of Challenge 3');
+console.log("End of Challenge 3");
 // */// (do not alter this line)
-
-
-
 
 /////////////////
 //             //
@@ -69,17 +67,11 @@ console.log('End of Challenge 3');
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 4');
+console.log("Start of Challenge 4");
 // ...your code below
 
-
-
-
-
-console.log('End of Challenge 4');
+console.log("End of Challenge 4");
 // */// (do not alter this line)
-
-
 
 /////////////////
 //             //
@@ -88,18 +80,13 @@ console.log('End of Challenge 4');
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 5');
+console.log("Start of Challenge 5");
 // ...your code below
 
-
-
-
 // ...your code above
-document.body.style.background = '#def';
-console.log('End of Challenge 3');
+document.body.style.background = "#def";
+console.log("End of Challenge 3");
 // */// (do not alter this line)
-
-
 
 /////////////////
 //             //
@@ -108,21 +95,16 @@ console.log('End of Challenge 3');
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 6');
+console.log("Start of Challenge 6");
 var dataReceived;
 
 function ajaxSimulate(id, callback) {
-  var database = ['Aaron', 'Barbara', 'Chris'];
-
+  var database = ["Aaron", "Barbara", "Chris"];
 }
 // ...your code below
 
-
-
-console.log('End of Challenge 6');
+console.log("End of Challenge 6");
 // */// (do not alter this line)
-
-
 
 /////////////////
 //             //
@@ -131,17 +113,11 @@ console.log('End of Challenge 6');
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 7');
+console.log("Start of Challenge 7");
 // ...your code below
 
-
-
-
-
-console.log('End of Challenge 7');
+console.log("End of Challenge 7");
 // */// (do not alter this line)
-
-
 
 /////////////////
 //             //
@@ -150,18 +126,11 @@ console.log('End of Challenge 7');
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 8');
+console.log("Start of Challenge 8");
 // ...your code below
 
-
-
-
-
-
-console.log('End of Challenge 8');
+console.log("End of Challenge 8");
 // */// (do not alter this line)
-
-
 
 /////////////////
 //             //
@@ -170,15 +139,8 @@ console.log('End of Challenge 8');
 /////////////////
 
 // /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
-console.log('Start of Challenge 9');
+console.log("Start of Challenge 9");
 // ...your code below
 
-
-
-
-
-
-
-console.log('End of Challenge 9');
+console.log("End of Challenge 9");
 // */// (do not alter this line)
-
